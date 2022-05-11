6530757159ff32d66d24645e6b5b94114a6e70f4
Tony Bavalan <42758825+tonybavalan@users.noreply.github.com>
2022-05-11 05:51:17 +0530

    initial commit via github desktop
    initial setup with codebase.

---
+++ src/components/HeaderVue.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <ButtonVue text="Add Task" color="green" />
+    </header>
+</template>
+
+<script>
+    import ButtonVue from './ButtonVue'
+
+    export default {
+        name: 'HeaderVue',
+        props:{
+            title: String,
+        },
+        components: {
+            ButtonVue,
+        },
+    }
+</script>
+
+<style scoped>
+    header {
+        display:flex;
+        justify-content:space-between;
+        align-items:center;
+        margin-bottom:20px;
+    }
+</style>
